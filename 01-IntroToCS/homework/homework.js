739981f84af18001a9bc7c1b956d5b1e42f37b7c
Gina <gjuarez@sw1848.com>
2022-07-05 11:59:11 -0500

    first commit

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -3,10 +3,30 @@
 function BinarioADecimal(num) {
   // tu codigo aca
 
+  var dec=num.split("");
+  dec=dec.reverse();
+
+
+
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+
+  var bin=[];
+
+for (var i=0; i<100 ; i++){
+
+  bin.unshift(num%2);
+  num=Math.floor(num/2);
+
+  if (num===1){
+    bin.unshift(num%2);
+    i=100;
+  }
+  }
+
+  return bin.join('');
 
 }
 
